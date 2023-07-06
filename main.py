9fa87ee9af0dad0e974a043f3d9a5f972883fedc
BluerZu <superluis222@hotmail.com>
2023-07-06 00:08:05 -0500

    OPENAI Datos históricos según fecha

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def inference_endpoint(doc: Document):
 
 # Cambiamos el puerto por defecto
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=7888)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
 
 
 
--- openaitest.py
+++ openaitest.py
@@ -1,33 +1,31 @@
 import openai
 from pydantic import BaseModel
 
-
-
-
 class Document(BaseModel):
     prompt: str = ''
 
-
 def inference(prompt: str) -> list:
     openai.organization = 'org-DYRwCOZ92Bsf4lIsYceu2SVQ'
-    openai.api_key = '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'
+    openai.api_key = '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'
     print('[PROCESANDO]'.center(40, '-'))
+    print(prompt)
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
+        temperature = 1,
         messages=[
-            {"role": "system", "content": """Eres un gran historiador, que me va a dar un dato relevante que haya pasado en la historia, de acuerdo.
-            a la fecha que se te envíe.
-            E.G
-            10 de Agosto
-            Dato relevante en la historia:
-            Primer grito de la independencia de Quito.
+            {"role": "system", "content": """Eres una base de datos enorme, que contiene toda la información histórica de la
+            humanidad, cuando se te envíe una fecha, responderás con un dato histórico relevante que haya ocurrido en dicha fecha.
+            E.G.
+            10 de agosto
+            Dato relevante: Día de la Independencia de Quito
             """},
             {"role": "user", "content": prompt}
-        ]
+        ],
+        max_tokens = 50
     )
     content = completion.choices[0].message.content
     total_tokens = completion.usage.total_tokens
-    print("Se han utilizado los siguientes tokens: " + total_tokens)
+    print("Se han utilizado los siguientes tokens: " + str(total_tokens))
     print('[SE TERMINÓ DE PROCESAR]'.center(40, '-'))
     return [content, total_tokens]
 
