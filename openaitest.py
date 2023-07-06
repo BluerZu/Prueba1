c002badce2b4729b5aa9b297a7b78a6fba546e06
BluerZu <superluis222@hotmail.com>
2023-07-05 22:11:44 -0500

    OPENAI como historiador

--- openaitest.py
+++ openaitest.py
@@ -1,8 +1,7 @@
 import openai
 from pydantic import BaseModel
 
-openai.organization = 'org-Cbvcdr6eNau56Sbr39n9QGvY'
-openai.api_key = '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'
+
 
 
 class Document(BaseModel):
@@ -10,15 +9,20 @@ class Document(BaseModel):
 
 
 def inference(prompt: str) -> list:
+    openai.organization = 'org-DYRwCOZ92Bsf4lIsYceu2SVQ'
+    openai.api_key = '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'
     print('[PROCESANDO]'.center(40, '-'))
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        temperature = 0.9,
-        context = "Estas en un aula de clases",
         messages=[
-          {"role": "system", "content": """Eres un contador de letras de las palabras que te envíen""",
-           },
-          {"role": "user", "content": prompt}
+            {"role": "system", "content": """Eres un gran historiador, que me va a dar un dato relevante que haya pasado en la historia, de acuerdo.
+            a la fecha que se te envíe.
+            E.G
+            10 de Agosto
+            Dato relevante en la historia:
+            Primer grito de la independencia de Quito.
+            """},
+            {"role": "user", "content": prompt}
         ]
     )
     content = completion.choices[0].message.content
